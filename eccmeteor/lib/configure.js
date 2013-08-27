18c363c7bf343141310cef97a275a55c8de95a78
huyinghuan <ec.huyinghuan@gmail.com>
2013-08-27 16:10:22 +0800

    补充提交

---
+++ eccmeteor/lib/configure.js
@@ -0,0 +1,10 @@
+/*
+*配置默认参数
+*/
+EccMeteorGlobelConfigure={
+	LoggerSetting:{
+      method4Server:"log2txt",//设置默认服务端的日志记录方式 [log2txt,log2terminal]
+      method4Client:"log2Console",//设置客户端控制台打印格式
+      logRate:true  //日志是否分级显示(waring,error,normal分文件记录) 默认为true
+	}
+}
