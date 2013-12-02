abc65364204d2200676128e65f9c69e0ff285b32
renjiea <jie.ren@dragonflow.com>
2013-12-02 10:07:30 +0800

    add

--- eccmeteor/server/startup/initSettingNodes.js
+++ eccmeteor/server/startup/initSettingNodes.js
@@ -34,8 +34,8 @@ initSettingNodes = function(status){
 			{id:37,pId:3,name:"time",action:"time",type:"report",icon:"imag/setting/time.png"},
 			{id:38,pId:3,name:"monitorInfo",action:"monitorInfo",type:"report",icon:"imag/setting/monitorInfo.png"},
 			{id:39,pId:3,name:"SysLogQuery",action:"SysLogQuery",type:"report",icon:"imag/setting/SysLogQuery.png"},
-			/*任杰 add SysLogsetting*/
 			{id:25,pId:2,name:"SysLogsetting",action:"SysLogsetting",type:"setting",icon:"imag/setting/SysLogsetting.png"},
+			/*任杰 add SysLogsetting*/
 		];
 	var length = setting.length;
 	for(var index = 0 ; index < length; index++){
