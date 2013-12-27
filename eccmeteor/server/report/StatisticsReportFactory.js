4216413b3bc44709b5a23367212b80de637d7666
ec.huyinghuan@gmail.com <ec.huyinghuan@gmail.com>
2013-12-27 18:31:07 +0800

    tmp6

--- eccmeteor/server/report/StatisticsReportFactory.js
+++ eccmeteor/server/report/StatisticsReportFactory.js
@@ -1,11 +1,33 @@
 
 StatisticsReportFactory = function(reportConfigureId){
-	this.init();
+	this.setting = {};
+	this.monitorIds = [];
+	this.extentDate = [];
+	this.init(reportConfigureId);
 };
 
-StatisticsReportFactory.prototype.init = function(){};
+StatisticsReportFactory.prototype.init = function(reportConfigureId){
+	this.setting = SvseStatisticalresultlist.findOne(reportConfigureId);
+
+};
+
+
 StatisticsReportFactory.prototype.getReportConfigure = function(){};
 
 
 StatisticsReportFactory.prototype.getSendEmailConfigure = function(){};
 
+StatisticsReportFactory.prototype.setExtentDate = function(setting){
+	var dateType = setting.Period ;
+	var today = new Date();
+
+	switch(dateType){
+		case "Day" : ;
+	}
+
+};
+
+StatisticsReportFactory.prototype.getExtentDate = function(){};
+
+
+StatisticsReportFactory.prototype.getMonitorIds = function(){};
